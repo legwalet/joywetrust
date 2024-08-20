823fb1e5baafc6b32073037f44bd7aa4b0300387
Tshegofatso Legwale <71738517+legwalet@users.noreply.github.com>
2024-08-20 20:47:56 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -6,10 +6,10 @@ define('DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'wordpress');
 define('DB_USER', getenv('WORDPRESS_DB_USER') ?: 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'password');
+define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'passwd');
 
 /** MySQL hostname */
-define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'localhost');
+define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'db');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
@@ -24,14 +24,14 @@ define('DB_COLLATE', '');
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
  * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
  */
-define('AUTH_KEY',         getenv('AUTH_KEY') ?: 'put your unique phrase here');
-define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY') ?: 'put your unique phrase here');
-define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY') ?: 'put your unique phrase here');
-define('NONCE_KEY',        getenv('NONCE_KEY') ?: 'put your unique phrase here');
-define('AUTH_SALT',        getenv('AUTH_SALT') ?: 'put your unique phrase here');
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT') ?: 'put your unique phrase here');
-define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT') ?: 'put your unique phrase here');
-define('NONCE_SALT',       getenv('NONCE_SALT') ?: 'put your unique phrase here');
+define('AUTH_KEY',         '1K|ef}X^|~!D:&6`{xC5 [-n)MDRC!QjgNbp.O7$H0!k*sTtJ052+6|$C?,^Ru7l');
+define('SECURE_AUTH_KEY',  '6c^LEF7vS,1fyy=H-L%U40CzxDK<QJ9~T+<AHI|Gz[d!-zjT9~5Ro*=X87rzz(Ca');
+define('LOGGED_IN_KEY',    '@fJmA^FhBt%+x>wjy6h?];Y<tib6kMG~E^!FA?8M=+fzf9-Va(ON|~Sxp/l)9J6`');
+define('NONCE_KEY',        ' c<;YE*dty<5r/n]-Fdt9lP.(aat?hOe{z+[3zP|$jk`F` j=~u~kB~/kZmK-T[q');
+define('AUTH_SALT',        '9b7fuf+I^h1U+^>mLdl}!25W/Z)6|81-wdch}ccG-|P&y!M!-UnQa/d:[q0/F(Eh');
+define('SECURE_AUTH_SALT', 'woP s),,m..>t.8_q=K<^LgvR@A1a_X9=k0l#9dJ;l9!`.|+pW]1hQ8H1UiaBF>2');
+define('LOGGED_IN_SALT',   ']P|AM|kv5XWz#|dxM5<]2L0+Kq5;Lt(dHf@!:A-:CFw6|l~Z_p`SoPeVX2wT|b@/');
+define('NONCE_SALT',       '1$)L|/G>93UCgrd:gZ`vY(YY}SMT:4|V)<=H(~4SsyHoElc(PjK-&!nmWCgWCS2v');
 
 /**#@-*/
 
